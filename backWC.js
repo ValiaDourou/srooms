f62446ac5be1c95578b29cac7691ed40a7967ae2
ValiaDourou <valiadourou@gmail.com>
2023-07-18 22:09:35 +0300

    eftiaksa to na diavazei kala ta qr, allakse apo once se continuously kai apopthikevei device type kai id

--- backWC.js
+++ backWC.js
@@ -1,22 +1,67 @@
-function decodeOnce(codeReader, selectedDeviceId) {
-  codeReader.decodeFromInputVideoDevice(selectedDeviceId, 'video').then((result) => {
-    console.log(result.text)
-  }).catch((err) => {
-    console.error(err)
+function decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth) {
+  codeReader.decodeFromInputVideoDeviceContinuously(selectedDeviceId, 'video', async (result, err) => {
+    var statusDiv = document.getElementById('result');
+
+    if (result) {
+      var qr=result.text;
+    var url= "http://localhost:8080/api/device/info/"+qr;
+    const response = await  fetch(url, {method: 'GET', headers:{
+      'Content-Type': 'application/json',
+      'Accept':'application/json',
+      'X-Authorization': 'Bearer '+ token
+       }
+     })
+     if(response.status==200){
+    var obj = await response.json();
+     var formData = new FormData();
+     formData.append('dtype', obj.type);
+     formData.append('did', qr);
+     formData.append('act',2);
+     const response1 = await fetch('./tokens.php',{ method: 'POST', body: formData });
+     document.location.href = 'controls.html';
+     }
+
+    }
+
+    if (err) {
+      if (err instanceof ZXing.NotFoundException) {
+      }
+      if (err instanceof ZXing.ChecksumException) {
+        statusDiv.textContent="Not valid QR code.";
+      }
+
+      if (err instanceof ZXing.FormatException) {
+        statusDiv.textContent=='A code was found, but it was in a invalid format.';
+      }
+    }
   })
 }
 
-window.addEventListener('load', function () {
+window.addEventListener('load', async function () {
   let selectedDeviceId;
   const codeReader = new ZXing.BrowserQRCodeReader()
   console.log('ZXing code reader initialized')
+  var token;
+  var rtoken;
+  var uid;
+  var uauth;
+  var formData = new FormData();
+   formData.append('act',1);
+   const response = await fetch('./tokens.php',{ method: 'POST', body: formData });
+   var data = await response.json();
+   if(data.length>0){
+    token=data[0];
+    rtoken=data[1];
+    uid=data[2];
+    uauth=data[3];
+   }
 
   codeReader.getVideoInputDevices()
     .then((videoInputDevices) => {
       selectedDeviceId = videoInputDevices[0].deviceId
 
       document.getElementById('startButton').addEventListener('click', () => {
-          decodeOnce(codeReader, selectedDeviceId);
+          decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth);
 
         console.log(`Started decode from camera with id ${selectedDeviceId}`)
       })
--- tokens.php
+++ tokens.php
@@ -10,10 +10,17 @@
     $_SESSION['uid'] = $_POST['uid'];
     $_SESSION['uauth'] = $_POST['uauth'];
     }
-    else{
+    else if($act==1){
     $arr[]=$_SESSION['token'];
     $arr[]=$_SESSION['rtoken'];
+    $arr[]=$_SESSION['uid'];
+    $arr[]=$_SESSION['uauth'];
+
     echo json_encode($arr, JSON_UNESCAPED_UNICODE);
+    }
+    else{
+    $_SESSION['dtype'] = $_POST['dtype'];
+    $_SESSION['did'] = $_POST['did'];
     }
 
 ?>  
