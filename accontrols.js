e2686353b0486b48131da7ded7dcd5a4ab89483a
ValiaDourou <valiadourou@gmail.com>
2023-07-21 01:55:39 +0300

    :)

--- accontrols.js
+++ accontrols.js
@@ -1,55 +1,19 @@
-function decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth) {
-    codeReader.decodeFromInputVideoDeviceContinuously(selectedDeviceId, 'video', async (result, err) => {
-      var statusDiv = document.getElementById('result');
-  
-      if (result) {
-        statusDiv.textContent=" ";
-        var qr=result.text;
-      var url= "http://localhost:8080/api/device/info/"+qr;
-      const response = await  fetch(url, {method: 'GET', headers:{
-        'Content-Type': 'application/json',
-        'Accept':'application/json',
-        'X-Authorization': 'Bearer '+ token
-         }
-       })
-       if(response.status==200){
-      var obj = await response.json();
-       var formData = new FormData();
-       formData.append('dtype', obj.type);
-       formData.append('did', qr);
-       formData.append('act',2);
-       const response1 = await fetch('./tokens.php',{ method: 'POST', body: formData });
-       if(obj.type=='light'){
-       document.location.href = 'lightcontrols.html';
-       }
-       else{
-        console.log("hi");
-       }
-       }
-  
-      }
-  
-      if (err) {
-        if (err instanceof ZXing.NotFoundException) {
-        }
-        if (err instanceof ZXing.ChecksumException) {
-          statusDiv.textContent="Not valid QR code.";
-        }
-  
-        if (err instanceof ZXing.FormatException) {
-          statusDiv.textContent='A code was found, but it was in a invalid format.';
-        }
-      }
-    })
-  }
+import {
+  GestureRecognizer,
+  FilesetResolver
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0"
+let gestureRecognizer
+      let runningMode = "VIDEO"
+      let webcamRunning = false
+      const videoHeight = "360px"
+      const videoWidth = "480px"  
   
   window.addEventListener('load', async function () {
     var pH = document.getElementById('pH');
     var tH = document.getElementById('tH');
     var sname = document.getElementById('sname');    
-    let selectedDeviceId;
-    const codeReader = new ZXing.BrowserQRCodeReader()
-    console.log('ZXing code reader initialized')
+    
+    var clk;
     var token;
     var rtoken;
     var uid;
@@ -57,6 +21,9 @@ function decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth)
     var dtype;
     var did;
     var dname;
+    var mi=0;
+    var fi=0;
+    var temp;
     var formData = new FormData();
      formData.append('act',3);
      const response = await fetch('./tokens.php',{ method: 'POST', body: formData });
@@ -90,6 +57,7 @@ function decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth)
             mH.innerHTML=obj[i].value;
         }
         if(obj[i].key=='temperature'){
+          temp=parseInt(obj[i].value);
             tH.innerHTML=obj[i].value;
         }
         if(obj[i].key=='active'){
@@ -102,34 +70,197 @@ function decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth)
       }
       }
   
-    codeReader.getVideoInputDevices()
-      .then((videoInputDevices) => {
-        selectedDeviceId = videoInputDevices[0].deviceId
-  
-        document.getElementById('pH').addEventListener('click', () => {
-            decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth);
-  
-          console.log(`Started decode from camera with id ${selectedDeviceId}`)
-        })
-        document.getElementById('tH').addEventListener('click', () => {
-            decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth);
-  
-          console.log(`Started decode from camera with id ${selectedDeviceId}`)
-        })
-        if(uauth=='TENANT_ADMIN'){
-        document.getElementById('fH').addEventListener('click', () => {
-            decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth);
-  
-          console.log(`Started decode from camera with id ${selectedDeviceId}`)
-        })
-        document.getElementById('mH').addEventListener('click', () => {
-            decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth);
-  
-          console.log(`Started decode from camera with id ${selectedDeviceId}`)
+      const createGestureRecognizer = async () => {
+        const vision = await FilesetResolver.forVisionTasks(
+          "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+        )
+        gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+          baseOptions: {
+            modelAssetPath:
+              "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+            delegate: "GPU"
+          },
+          runningMode: runningMode
         })
       }
-      })
-      .catch((err) => {
-        console.error(err)
-      })
+      createGestureRecognizer()
+      
+      const video = document.getElementById("video")
+      const canvasElement = document.getElementById("output_canvas")
+      const canvasCtx = canvasElement.getContext("2d")
+      const gestureOutput = document.getElementById("gesture_output")
+      
+      // Check if webcam access is supported.
+      function hasGetUserMedia() {
+        return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
+      }
+      
+      // If webcam supported, add event listener to button for when user
+      // wants to activate it.
+      if (hasGetUserMedia()) {
+        document.getElementById('pH').addEventListener('click', () => {
+          clk=1;
+          if(webcamRunning===false){
+          enableCam();
+          }
+      });
+        document.getElementById('tH').addEventListener('click', () => {
+          clk=2;
+          if(webcamRunning===false){
+            enableCam();
+            }
+      });
+        if(uauth=='TENANT_ADMIN'){
+        document.getElementById('fH').addEventListener('click', () => {
+          clk=3;
+          setInterval(function() {
+            if(fi==1){
+            if(fH.innerHTML=="low"){
+              fH.innerHTML="mid";
+            }
+            else if(fH.innerHTML=="mid")
+            {
+              fH.innerHTML="high";
+            }
+            else{
+              fH.innerHTML="low";
+            }
+          }
+          fi=0;
+        }, 2000);
+          if(webcamRunning===false){
+            enableCam();
+            }
+      });
+      document.getElementById('mH').addEventListener('click', () => {
+        clk=4;
+        setInterval(function() {
+          if(mi==1){
+          if(mH.innerHTML=="dry"){
+            mH.innerHTML="cool";
+          }
+          else if(mH.innerHTML=="cool")
+          {
+            mH.innerHTML="heat";
+          }
+          else{
+            mH.innerHTML="dry";
+          }
+        }
+        mi=0;
+      }, 2000);
+        if(webcamRunning===false){
+          enableCam();
+          }
+    });
+        }
+      
+      } else {
+        console.warn("getUserMedia() is not supported by your browser")
+      }
+      
+      // Enable the live webcam view and start detection.
+      function enableCam() {
+        if (!gestureRecognizer) {
+          alert("Please wait for gestureRecognizer to load")
+          return
+        }
+      
+          webcamRunning = true
+      
+      
+        // getUsermedia parameters.
+        const constraints = {
+          video: true
+        }
+      
+        // Activate the webcam stream.
+        navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+          video.srcObject = stream
+          video.addEventListener("loadeddata", predictWebcam)
+        })
+      }
+      
+      let lastVideoTime = -1
+      let results = undefined
+      async function predictWebcam() {
+        const webcamElement = document.getElementById("video")
+        // Now let's start detecting the stream.
+      
+        let nowInMs = Date.now()
+        if (video.currentTime !== lastVideoTime) {
+          lastVideoTime = video.currentTime
+          results = gestureRecognizer.recognizeForVideo(video, nowInMs)
+        }
+      
+        canvasCtx.save()
+        canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height)
+      
+        canvasElement.style.height = videoHeight
+        webcamElement.style.height = videoHeight
+        canvasElement.style.width = videoWidth
+        webcamElement.style.width = videoWidth
+        if (results.landmarks) {
+          for (const landmarks of results.landmarks) {
+            drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+              color: "#00FF00",
+              lineWidth: 5
+            })
+            drawLandmarks(canvasCtx, landmarks, { color: "#FF0000", lineWidth: 2 })
+          }
+        }
+        canvasCtx.restore()
+        if (results.gestures.length > 0) {
+          gestureOutput.style.display = "inline-block"
+          gestureOutput.style.width = videoWidth
+          const categoryName = results.gestures[0][0].categoryName
+          gMovement(categoryName);
+          const categoryScore = parseFloat(
+            results.gestures[0][0].score * 100
+          ).toFixed(2)
+          gestureOutput.innerText = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore} %`
+        } else {
+          gestureOutput.style.display = "none"
+        }
+        // Call this function again to keep predicting when the browser is ready.
+        if (webcamRunning === true) {
+          window.requestAnimationFrame(predictWebcam)
+        }
+      }
+      
+      function gMovement(categoryName){
+        if(clk==1){
+          if(categoryName=="Open_Palm"){
+            pH.innerHTML="ON";
+          }
+          if(categoryName=="Closed_Fist"){
+            pH.innerHTML="OFF";
+          }
+        }
+        if(clk==2){
+          if(categoryName=="Thumb_Up"){
+            if(temp<35){
+            temp=temp+1;
+            tH.innerHTML=temp.toString();
+            }
+          }
+          if(categoryName=="Thumb_Down"){
+            if(temp>14){
+            temp=temp-1;
+            tH.innerHTML=temp.toString();
+            }
+          }
+        }
+        if(clk==3){
+          if(categoryName=="Victory"){
+            fi=1;
+          }
+        }
+        if(clk==4){
+          if(categoryName=="ILoveYou"){
+            mi=1;
+          }
+        }
+      }
+    
   })
--- speakcontrols.js
+++ speakcontrols.js
@@ -1,47 +1,12 @@
-function decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth) {
-    codeReader.decodeFromInputVideoDeviceContinuously(selectedDeviceId, 'video', async (result, err) => {
-      var statusDiv = document.getElementById('result');
-  
-      if (result) {
-        statusDiv.textContent=" ";
-        var qr=result.text;
-      var url= "http://localhost:8080/api/device/info/"+qr;
-      const response = await  fetch(url, {method: 'GET', headers:{
-        'Content-Type': 'application/json',
-        'Accept':'application/json',
-        'X-Authorization': 'Bearer '+ token
-         }
-       })
-       if(response.status==200){
-      var obj = await response.json();
-       var formData = new FormData();
-       formData.append('dtype', obj.type);
-       formData.append('did', qr);
-       formData.append('act',2);
-       const response1 = await fetch('./tokens.php',{ method: 'POST', body: formData });
-       if(obj.type=='light'){
-       document.location.href = 'lightcontrols.html';
-       }
-       else{
-        console.log("hi");
-       }
-       }
-  
-      }
-  
-      if (err) {
-        if (err instanceof ZXing.NotFoundException) {
-        }
-        if (err instanceof ZXing.ChecksumException) {
-          statusDiv.textContent="Not valid QR code.";
-        }
-  
-        if (err instanceof ZXing.FormatException) {
-          statusDiv.textContent='A code was found, but it was in a invalid format.';
-        }
-      }
-    })
-  }
+import {
+  GestureRecognizer,
+  FilesetResolver
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0"
+let gestureRecognizer
+      let runningMode = "VIDEO"
+      let webcamRunning = false
+      const videoHeight = "360px"
+      const videoWidth = "480px"  
   
   window.addEventListener('load', async function () {
     var pH = document.getElementById('pH');
@@ -49,9 +14,9 @@ function decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth)
     var sname = document.getElementById('sname');    
     var singer = document.getElementById('artistname');    
     var song = document.getElementById('songname');    
-    let selectedDeviceId;
-    const codeReader = new ZXing.BrowserQRCodeReader()
-    console.log('ZXing code reader initialized')
+    var image = document.getElementById('image');    
+
+    var clk;
     var token;
     var rtoken;
     var uid;
@@ -59,6 +24,8 @@ function decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth)
     var dtype;
     var did;
     var dname;
+    var vol;
+    var inter=0;
     var formData = new FormData();
      formData.append('act',3);
      const response = await fetch('./tokens.php',{ method: 'POST', body: formData });
@@ -87,6 +54,7 @@ function decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth)
         cs.src=obj[i].value;
         }
         if(obj[i].key=='volume'){
+          vol=parseInt(obj[i].value);
             vH.innerHTML=obj[i].value;
         }
         if(obj[i].key=='singer'){
@@ -95,6 +63,9 @@ function decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth)
       if(obj[i].key=='song'){
         song.innerHTML=obj[i].value;
     }
+      if(obj[i].key=='image'){
+       image.src=obj[i].value;
+     }
         if(obj[i].key=='active'){
         if(obj[i].value==false){
         pH.innerHTML="OFF";
@@ -105,30 +76,191 @@ function decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth)
         }
       }
       }
-  
-    codeReader.getVideoInputDevices()
-      .then((videoInputDevices) => {
-        selectedDeviceId = videoInputDevices[0].deviceId
-  
-        document.getElementById('pH').addEventListener('click', () => {
-            decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth);
-  
-          console.log(`Started decode from camera with id ${selectedDeviceId}`)
-        })
-        document.getElementById('vH').addEventListener('click', () => {
-            decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth);
-  
-          console.log(`Started decode from camera with id ${selectedDeviceId}`)
-        })
-        if(uauth=='TENANT_ADMIN'){
-        document.getElementById('cH').addEventListener('click', () => {
-            decodeContinuously(codeReader, selectedDeviceId,token,rtoken,uid,uauth);
-  
-          console.log(`Started decode from camera with id ${selectedDeviceId}`)
-        })
+
+const createGestureRecognizer = async () => {
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+  )
+  gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath:
+        "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+      delegate: "GPU"
+    },
+    runningMode: runningMode
+  })
+}
+createGestureRecognizer()
+
+const video = document.getElementById("video")
+const canvasElement = document.getElementById("output_canvas")
+const canvasCtx = canvasElement.getContext("2d")
+const gestureOutput = document.getElementById("gesture_output")
+
+// Check if webcam access is supported.
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
+}
+
+// If webcam supported, add event listener to button for when user
+// wants to activate it.
+if (hasGetUserMedia()) {
+  document.getElementById('pH').addEventListener('click', () => {
+    clk=1;
+    if(webcamRunning===false){
+    enableCam();
+    }
+});
+  document.getElementById('vH').addEventListener('click', () => {
+    clk=2;
+    if(webcamRunning===false){
+      enableCam();
       }
+});
+  if(uauth=='TENANT_ADMIN'){
+  document.getElementById('cH').addEventListener('click', () => {
+    clk=3;
+    setInterval(function() {
+      if(inter==1){
+      if(song.innerHTML=="1000 Kosmoi"){
+        cs.src="1000BLUNTS.mp3";
+        song.innerHTML="1000 Blunts";
+        singer.innerHTML="$uicideboy$";
+        image.src="1000BLUNTS.jpg";
+      }
+      else if(song.innerHTML=="1000 Blunts")
+      {
+        cs.src="ESCAPISM.mp3";
+        song.innerHTML="Escapism";
+        singer.innerHTML="RAYE";
+        image.src="ESCAPISM.jpg";
+
+      }
+      else{
+        cs.src="1000KOSMOI.mp3";
+        song.innerHTML="1000 Kosmoi";
+        singer.innerHTML="Dani Gambino";
+        image.src="1000kosmoi.jpg";
+
+      }
+      if(pH.innerHTML=="ON"){
+        cs.play();
+        }
+        else{
+        cs.pause();
+        }
+    }
+    inter=0;
+  }, 2000);
+    if(webcamRunning===false){
+      enableCam();
+      }
+});
+  }
+
+} else {
+  console.warn("getUserMedia() is not supported by your browser")
+}
+
+// Enable the live webcam view and start detection.
+function enableCam() {
+  if (!gestureRecognizer) {
+    alert("Please wait for gestureRecognizer to load")
+    return
+  }
+
+    webcamRunning = true
+
+
+  // getUsermedia parameters.
+  const constraints = {
+    video: true
+  }
+
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+    video.srcObject = stream
+    video.addEventListener("loadeddata", predictWebcam)
+  })
+}
+
+let lastVideoTime = -1
+let results = undefined
+async function predictWebcam() {
+  const webcamElement = document.getElementById("video")
+  // Now let's start detecting the stream.
+
+  let nowInMs = Date.now()
+  if (video.currentTime !== lastVideoTime) {
+    lastVideoTime = video.currentTime
+    results = gestureRecognizer.recognizeForVideo(video, nowInMs)
+  }
+
+  canvasCtx.save()
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height)
+
+  canvasElement.style.height = videoHeight
+  webcamElement.style.height = videoHeight
+  canvasElement.style.width = videoWidth
+  webcamElement.style.width = videoWidth
+  if (results.landmarks) {
+    for (const landmarks of results.landmarks) {
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+        color: "#00FF00",
+        lineWidth: 5
       })
-      .catch((err) => {
-        console.error(err)
-      })
+      drawLandmarks(canvasCtx, landmarks, { color: "#FF0000", lineWidth: 2 })
+    }
+  }
+  canvasCtx.restore()
+  if (results.gestures.length > 0) {
+    gestureOutput.style.display = "inline-block"
+    gestureOutput.style.width = videoWidth
+    const categoryName = results.gestures[0][0].categoryName
+    gMovement(categoryName);
+    const categoryScore = parseFloat(
+      results.gestures[0][0].score * 100
+    ).toFixed(2)
+    gestureOutput.innerText = `GestureRecognizer: ${categoryName}\n Confidence: ${categoryScore} %`
+  } else {
+    gestureOutput.style.display = "none"
+  }
+  // Call this function again to keep predicting when the browser is ready.
+  if (webcamRunning === true) {
+    window.requestAnimationFrame(predictWebcam)
+  }
+}
+
+function gMovement(categoryName){
+  if(clk==1){
+    if(categoryName=="Open_Palm"){
+      pH.innerHTML="ON";
+      cs.play();
+    }
+    if(categoryName=="Closed_Fist"){
+      pH.innerHTML="OFF";
+      cs.pause();
+    }
+  }
+  if(clk==2){
+    if(categoryName=="Thumb_Up"){
+      if(vol<100){
+      vol=vol+1;
+      vH.innerHTML=vol.toString();
+      }
+    }
+    if(categoryName=="Thumb_Down"){
+      if(vol>0){
+      vol=vol-1;
+      vH.innerHTML=vol.toString();
+      }
+    }
+  }
+  if(clk==3){
+    if(categoryName=="Victory"){
+      inter=1;
+      
+    }
+  }
+}
   })
